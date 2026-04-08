48c78682b1bb0958d775304e4b1dc5fa38bc9bb2
Chopinote <ZhanhaoYan0512@gmail.com>
2026-04-08 13:05:49 +0000

    Deploying to gh-pages from @ Chopinote/Chopinote.github.io@006357c851ba1c6676bbe45925d2246cf967e97c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/misc/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
